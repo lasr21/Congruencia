10cdcd642f5331ad5d34eec2e99a3ef2b43eae01
Jose Luis Dupinet <jldupinet@gmail.com>
2011-10-13 20:24:33 -0500

    Agregada funcion para sacar el maximo comun divisor

--- congruencias.c
+++ congruencias.c
@@ -1,13 +1,19 @@
-#include stdio.h
+#include <stdio.h>
 
 int gcd(int a, int b){
-	
-	
+	int residuo = 1;
+
+	while(residuo!=0){
+		residuo = a%b;
+		a=b;
+		b=residuo;
+	}
+	return a;
 }
 
 int main (){
 	
-
+	printf("GCD:%i\n",gcd(48,18));
 
 
 }
